262965f5d0f16b3fc8fabad3c4a5e44bf80733c8
JGBrown <j.grant.brown@gmail.com>
2017-12-06 16:15:05 -0500

    fix routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
 
-  root '/', :to => 'application#home'
+  root :to => 'application#home'
   get 'about', :to => 'application#about'
 
   resources :natparks, only: [:index, :show]
